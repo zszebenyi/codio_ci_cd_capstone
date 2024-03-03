3a7052c05dc9f95978d5473d0e1c575c58ddb74d
Zoltán Szebenyi <zszebenyi@google.com>
2024-03-03 20:07:04 +0000

    Small fixes.

--- src/js/shuffle.js
+++ src/js/shuffle.js
@@ -1,11 +1,9 @@
-function shuffle(a) {
+function shuffle (a) {
   for (let i = a.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [a[i], a[j]] = [a[j], a[i]];
+    [a[i], a[j]] = [a[j], a[i]]
   }
-  return a;
+  return a
 }
 
-
-
-module.exports = shuffle; //This should be the last line in the file
+module.exports = shuffle // This should be the last line in the file
